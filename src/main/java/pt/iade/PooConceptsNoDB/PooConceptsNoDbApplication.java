21f40b9e38d4dc551da27caa7c81efc6f433fa35
Filipe Dávila <filipedavila6@gmail.com>
2020-11-10 16:42:34 +0000

    Commit do dos ficheiros base para a framework Spring Boot

---
+++ src/main/java/pt/iade/PooConceptsNoDB/PooConceptsNoDbApplication.java
@@ -0,0 +1,13 @@
+package pt.iade.PooConceptsNoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooConceptsNoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooConceptsNoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/iade/PooConceptsNoDB/PooConceptsNoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.iade.PooConceptsNoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooConceptsNoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
